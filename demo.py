8d15cbed8fa46002075413ab10260db97520a0ac
Son Do <63930958+skd862@users.noreply.github.com>
2022-03-09 17:55:56 -0600

    Online

---
+++ demo.py
@@ -0,0 +1,119 @@
+import folium
+import folium.plugins as plugins
+import streamlit as st
+from streamlit_folium import folium_static
+import xarray as xr
+from syn_sar import *
+
+# Page Configuration
+st.set_page_config(layout="wide")
+
+# Title and Description
+st.title("Forecasting Inundation Extents using REOF Analysis (FIER)-Bangladesh")
+
+
+row1_col1, row1_col2 = st.columns([2, 1])
+# Set up Geemap
+with row1_col1:
+    m = folium.Map(
+        zoom_start=7,
+        location =(23.68,90.35),
+        control_scale=True,
+    )
+    plugins.Fullscreen(position='topright').add_to(m)
+    folium.TileLayer('Stamen Terrain').add_to(m)
+    folium.LayerControl().add_to(m)
+
+with row1_col2:
+    # Form
+    with st.form('Perform REOF Analysis'):
+        # lAT / LONG
+        station = st.selectbox(
+     'Determine station:',
+     ('SW72','uwu' ),
+     )
+
+        resolution = st.select_slider(
+     'Select resolution',
+     options=['100m','500m',],
+     )
+
+        lw = st.number_input("Water level:",
+        value = 5.0,
+        format = '%.3f'
+        )
+
+
+        # Submit Button
+        submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            if station == 'SW72':
+
+                st.write('Station: ',station)
+                st.write('resolution: ',resolution)
+                st.write('Water level (m): %.3f'%(lw))
+
+                location = [24.3, 91]
+                m = folium.Map(
+                    zoom_start = 9,
+                    location = location,
+                    control_scale=True,
+                )
+
+
+                RSM = xr.open_dataset('Bangladesh/RSM/' + station + '/RSM_hydro.nc')
+                bounds = [[RSM.lat.values.min(), RSM.lon.values.min()], [RSM.lat.values.max(), RSM.lon.values.max()]]
+
+                # Add RSM
+                for i in range(1,5):
+                    image_name = 'Bangladesh/RSM/' + station + '/RSM_%s.png'%(i)
+                    folium.raster_layers.ImageOverlay(
+                        image= image_name,
+                        bounds = bounds,
+                        opacity = 0.7,
+                        name = 'RSM ' + str(i),
+                        show = False
+                    ).add_to(m)
+
+                # Add SYN_SAR Image
+                image_folder = image_output(station, float(lw))
+
+                folium.raster_layers.ImageOverlay(
+                    image= image_folder +'/syn_sar.png',
+                    bounds = bounds,
+                    opacity = 0.5,
+                    name = 'Synthesized Sar Image_' + station + '_' + str(lw),
+                    show = False
+                ).add_to(m)
+
+                # Add Z_SCORE
+                folium.raster_layers.ImageOverlay(
+                    image= image_folder +'/z_score.png',
+                    bounds = bounds,
+                    opacity = 0.5,
+                    name = 'Z-score Image_' + station + '_' + str(lw),
+                    show = False
+                ).add_to(m)
+
+                # Add Inundation
+                folium.raster_layers.ImageOverlay(
+                    image= image_folder +'/water_map.png',
+                    bounds = bounds,
+                    opacity = 0.5,
+                    name = 'Inundation Map_' + station + '_' + str(lw),
+                    show = True
+                ).add_to(m)
+
+                plugins.Fullscreen(position='topright').add_to(m)
+                folium.TileLayer('Stamen Terrain').add_to(m)
+                folium.LayerControl().add_to(m)
+
+
+with row1_col1:
+    folium_static(m, height = 600, width = 800)
+    st.markdown(
+    """
+    Reference: Chang, C. H., Lee, H., Kim, D., Hwang, E., Hossain, F., Chishtie, F., ... & Basnayake, S. (2020). Hindcast and forecast of daily inundation extents using satellite SAR and altimetry data with rotated empirical orthogonal function analysis: Case study in Tonle Sap Lake Floodplain. Remote Sensing of Environment, 241, 111732.
+    """
+    )
---
+++ syn_sar.py
@@ -0,0 +1,89 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow.keras import models
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.colors
+
+root_output_folder = 'Bangladesh/'
+
+def tpc_predict(site, mode, value):
+    tpc_mode = 'TF_model/site-%s_tpc%s'%(str(site), str(mode).zfill(2))
+
+    in_model = models.load_model(root_output_folder + tpc_mode)
+
+    return in_model.predict([value])[0][0]
+
+def synthesize_sar(site, value,):
+
+    sm_mode = 'RSM/%s/RSM_hydro.nc'%(site)
+    RSM = xr.open_dataset(root_output_folder + sm_mode)
+    for ct_mode in range(len(RSM.mode.values)):
+        sm = RSM.spatial_modes.values[:,:,ct_mode]
+        est_tpc = tpc_predict(site, ct_mode + 1, value)
+        if ct_mode==0:
+            syn_sar = sm*est_tpc
+        else:
+            syn_sar = syn_sar + sm*est_tpc
+
+    all_meanVV_dir = 'stats_img/%s/all_meanVV.nc'%(site)
+    all_meanVV = xr.open_dataset(root_output_folder + all_meanVV_dir)
+    all_meanVV = all_meanVV.to_array().values[0,:,:]
+    syn_sar = syn_sar + all_meanVV
+
+    # Z-score
+    zscore_threshold = -3
+
+    dry_meanVV_dir = 'stats_img/%s/dry_meanVV.nc'%(site)
+    dry_meanVV = xr.open_dataset(root_output_folder + dry_meanVV_dir)
+    dry_meanVV = dry_meanVV.to_array().values[0,:,:]
+
+    dry_stdVV_dir = 'stats_img/%s/dry_stdVV.nc'%(site)
+    dry_stdVV = xr.open_dataset(root_output_folder + dry_stdVV_dir)
+    dry_stdVV = dry_stdVV.to_array().values[0,:,:]
+
+    z_score_img = (syn_sar-dry_meanVV)/dry_stdVV
+
+    # Inundation Map
+
+    aoi_indx = np.argwhere(~np.isnan(RSM.spatial_modes.values))
+    water_indx = np.argwhere( z_score_img < zscore_threshold )
+    water_map = np.empty((z_score_img.shape[0], z_score_img.shape[1]))
+    water_map[:] = np.nan
+    water_map[aoi_indx[:,0], aoi_indx[:,1]] = 0
+    water_map[water_indx[:,0], water_indx[:,1]] = 1
+
+    # water_map = z_score_img.copy()
+    # water_map[water_map < zscore_threshold] = 1
+    # water_map[water_map >= zscore_threshold] = 0
+    
+    return syn_sar, z_score_img, water_map
+
+def image_output(site, value):
+
+    folder_name = 'output/' + site + '_%.3f'%(value)
+    if not os.path.isdir(folder_name):
+        os.makedirs(folder_name)
+        sar_image, z_score_image, water_map_image = synthesize_sar(site, round(value,3))
+
+        fig = plt.imshow(sar_image, cmap='gray')
+        plt.axis('off')
+        plt.savefig(folder_name +'/syn_sar.png', bbox_inches='tight', dpi=300)
+
+        fig = plt.imshow(z_score_image, cmap='jet', vmin=-3, vmax=3, interpolation='None')
+        plt.savefig(folder_name +'/z_score.png', bbox_inches='tight', dpi=300)
+        print('Created', folder_name)
+
+        water_cmap =  matplotlib.colors.ListedColormap(["silver","darkblue"])
+        fig = plt.imshow(water_map_image, cmap = water_cmap)
+        plt.clim(vmin=-0.5, vmax=1.5)
+        plt.axis('off')
+        plt.savefig(folder_name +'/water_map.png', bbox_inches='tight', dpi=300, interpolation='None')
+
+    else:
+        print(folder_name,'existed')
+
+    return folder_name #Directory of Synthesize Image
